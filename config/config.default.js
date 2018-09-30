83b0cfb2bf1ddfcd74a09bef7cfea353f79d7618
wangweianger <752636052@qq.com>
2018-09-30 15:11:39 +0800

    mongodb主从架构本地环境搭建并配置egg-mongoose

--- config/config.default.js
+++ config/config.default.js
@@ -35,20 +35,10 @@ module.exports = appInfo => {
         client: {
             port: 6379, // Redis port
             host: '127.0.0.1', // Redis host
-            password: 'auth',
+            password: '',
             db: 0,
         },
     };
-
-    // mongoose配置
-    // config.mongoose = {
-    //     client: {
-    //         url: 'mongodb://127.0.0.1:27017/performance_1',
-    //         options: {
-    //             server: { poolSize: 20 },
-    //         },
-    //     },
-    // };
 
     // mongoose配置
     config.mongoose = {
