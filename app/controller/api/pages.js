00876939fc15f052832d4c36b314006c46f92069
wangweianger <752636052@qq.com>
2018-10-12 14:02:26 +0800

    新增域名，城市，浏览器，用户系统查询条件

--- app/controller/api/pages.js
+++ app/controller/api/pages.js
@@ -9,15 +9,10 @@ class PagesController extends Controller {
         const { ctx } = this;
         const query = ctx.request.query;
         const appId = query.appId;
-        const type = query.type || 1;
-        const pageNo = query.pageNo ||1 ;
-        const pageSize = query.pageSize || this.app.config.pageSize;
-        const beginTime = query.beginTime;
-        const endTime = query.endTime;
 
         if (!appId) throw new Error('平均页面性能列表：appId不能为空');
 
-        const result = await ctx.service.web.webPages.getAveragePageList(appId, type, pageNo, pageSize, beginTime, endTime);
+        const result = await ctx.service.web.webPages.getAveragePageList(ctx);
 
         ctx.body = this.app.result({
             data: result,
--- app/service/web/web_pages.js
+++ app/service/web/web_pages.js
@@ -6,24 +6,65 @@ const Service = require('egg').Service;
 class PvuvivService extends Service {
 
     // 获得页面性能数据平均值
-    async getAveragePageList(appId, type, pageNo, pageSize, beginTime, endTime) {
+    async getAveragePageList(ctx) {
+        const query = ctx.request.query;
+        const appId = query.appId;
+        let type = query.type || 1;
+        let pageNo = query.pageNo || 1;
+        let pageSize = query.pageSize || this.app.config.pageSize;
+        const beginTime = query.beginTime;
+        const endTime = query.endTime;
+        const url = query.url;
+        const city = query.city;
+        const isCity = query.isCity;
+        const isBrowser = query.isBrowser;
+        const isSystem = query.isSystem;
+
         pageNo = pageNo * 1;
         pageSize = pageSize * 1;
         type = type * 1;
-        
-        const query = { $match: { app_id: appId, speed_type: type } };
-        if (beginTime && endTime)
-            query.$match.create_time = { $gte: new Date(beginTime), $lte: new Date(endTime) };
+
+        // 查询参数拼接
+        const queryjson = { $match: { app_id: appId, speed_type: type }, }
+        if (url) queryjson.$match.url = url;
+        if (city) queryjson.$match.city = city;
+        if (beginTime && endTime) queryjson.$match.create_time = { $gte: new Date(beginTime), $lte: new Date(endTime) };
+
+        const lookup = {
+            $lookup: {
+                from: "webenvironments",
+                localField: "mark_page",
+                foreignField: "mark_page",
+                as: "fromItems"
+            }
+        }
+        const group_id = { 
+            url: "$url", 
+            city: `${isCity == 'true' ?"$city":""}`, 
+            browser: `${isBrowser == 'true' ? "$browser" : ""}`, 
+            system: `${isSystem == 'true' ? "$system" : ""}`, 
+        };
+
         // 请求总条数
         const count = await this.ctx.model.Web.WebPages.aggregate([
-            query,
-            {$group: { _id: "$url",count: { $sum: 1 },}},
+            lookup,
+            { $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: ["$fromItems", 0] }, "$$ROOT"] } } },
+            { $project: { fromItems: 0 } },
+            queryjson,
+            {
+                $group: { 
+                    _id: group_id,
+                    count: { $sum: 1 },
+                }
+            },
         ]).exec();
-        // 请求分页数据
         const datas = await this.ctx.model.Web.WebPages.aggregate([
-            query,
+            lookup,
+            { $replaceRoot: { newRoot: { $mergeObjects: [{ $arrayElemAt: ["$fromItems", 0] }, "$$ROOT"] } } },
+            { $project: { fromItems: 0 } },
+            queryjson,
             { $group: { 
-                _id: "$url", 
+                _id: group_id,
                 count: { $sum: 1 }, 
                 load_time: { $avg: "$load_time" }, 
                 dns_time: { $avg: "$dns_time" }, 
