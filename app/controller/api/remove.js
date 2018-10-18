f75a91c787f67962d9a3d5fffcc4f2751e3357a4
wangweianger <752636052@qq.com>
2018-10-18 21:13:01 +0800

    性能优化

---
+++ app/controller/api/remove.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class AjaxsController extends Controller {
+
+    // 清空db1 1日之前无用数据
+    async deleteDb1WebData() {
+        const { ctx } = this;
+
+        const result = await ctx.service.remove.deleteDb1WebData();
+        ctx.body = this.app.result({
+            data: result,
+        });
+    }
+
+    // 清空db2 number日之前所有性能数据
+    async deleteDb2WebData() {
+        const { ctx } = this;
+        const query = ctx.request.body;
+        const number = query.number || 10;
+
+        const result = await ctx.service.remove.deleteDb2WebData(number);
+        ctx.body = this.app.result({
+            data: result,
+        });
+    }
+
+}
+
+module.exports = AjaxsController;
---
+++ app/service/remove.js
@@ -0,0 +1,42 @@
+'use strict';
+const parser = require('cron-parser');
+const Service = require('egg').Service;
+
+class RemoveService extends Service {
+
+
+    // 定时删除原始上报数据 一天删一次
+    async deleteDb1WebData() {
+        const interval = parser.parseExpression(this.app.config.pvuvip_task_day_time);
+        interval.prev();
+        interval.prev();
+        const endTime = new Date(interval.prev().toString());
+
+        const query = { create_time: { $lt: endTime } };
+
+        return await this.ctx.model.Web.WebReport.remove(query).exec();
+    }
+
+    // 清空db2 number日之前所有性能数据
+    async deleteDb2WebData(number) {
+        number = number * 1;
+        const interval = parser.parseExpression(this.app.config.pvuvip_task_day_time);
+        const endTime = new Date(new Date(interval.prev().toString()).getTime() - number * 86400000);
+        const query = { create_time: { $lt: endTime } };
+
+        // Ajax
+        const remove1 = Promise.resolve(this.ctx.model.Web.WebAjaxs.remove(query));
+        // Pages
+        const remove2 = Promise.resolve(this.ctx.model.Web.WebPages.remove(query));
+        // Environment
+        const remove3 = Promise.resolve(this.ctx.model.Web.WebEnvironment.remove(query));
+        // Errors
+        const remove4 = Promise.resolve(this.ctx.model.Web.WebErrors.remove(query));
+        // Resource
+        const remove5 = Promise.resolve(this.ctx.model.Web.WebResource.remove(query));
+
+        return await Promise.all([ remove1, remove2, remove3, remove4, remove5 ]);
+    }
+}
+
+module.exports = RemoveService;
