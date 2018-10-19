021d4f8334d6e692b7803bf6771b816beefa6241
wangweianger <752636052@qq.com>
2018-10-19 22:13:55 +0800

    性能优化，多线程

--- app/service/web/web_ajaxs.js
+++ app/service/web/web_ajaxs.js
@@ -54,6 +54,64 @@ class AjaxsService extends Service {
             method:"$method",
         };
 
+        return url ? await this.oneThread(queryjson, pageNo, pageSize, group_id) 
+            : await this.moreThread(appId, type, beginTime,endTime,queryjson, pageNo, pageSize, group_id);
+    }
+
+    // 平均求值数多线程
+    async moreThread(appId, type, beginTime,endTime,queryjson, pageNo, pageSize, group_id){
+        const result = [];
+        let distinct = await this.ctx.model.Web.WebAjaxs.distinct('url', queryjson.$match) || [];
+        let copdistinct = distinct;
+
+        const betinIndex = (pageNo - 1) * pageSize;
+        if (distinct && distinct.length) {
+            distinct = distinct.slice(betinIndex, betinIndex + pageSize);
+        }
+        const resolvelist = [];
+        for (let i = 0, len = distinct.length; i < len; i++) {
+            queryjson.$match.url = distinct[i];
+            resolvelist.push(
+                Promise.resolve(
+                    this.ctx.model.Web.WebAjaxs.aggregate([
+                        { $match: { app_id: appId, url: distinct[i], speed_type: type, create_time: { $gte: new Date(beginTime), $lte: new Date(endTime) } } },
+                        {
+                            $group: {
+                                _id: group_id,
+                                count: { $sum: 1 },
+                                duration: { $avg: "$duration" },
+                                body_size: { $avg: "$decoded_body_size" },
+                            }
+                        },
+                    ])
+                )
+            )
+        }
+        const all = await Promise.all(resolvelist) || [];
+        all.forEach(item => {
+            result.push(item[0]);
+        })
+        result.sort(function (obj1, obj2) {
+            let val1 = obj1.count;
+            let val2 = obj2.count;
+            if (val1 < val2) {
+                return 1;
+            } else if (val1 > val2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } );
+
+        return {
+            datalist: result,
+            totalNum: copdistinct.length,
+            pageNo: pageNo,
+        };
+    }
+
+    // 单个api接口查询平均信息
+    async oneThread(queryjson, pageNo, pageSize, group_id) {
         const count = Promise.resolve(this.ctx.model.Web.WebAjaxs.distinct('url', queryjson.$match));
         const datas = Promise.resolve(
             this.ctx.model.Web.WebAjaxs.aggregate([
@@ -72,7 +130,6 @@ class AjaxsService extends Service {
             ])
         );
         const all = await Promise.all([count, datas]);
-
         return {
             datalist: all[1],
             totalNum: all[0].length,
--- app/service/web/web_pages.js
+++ app/service/web/web_pages.js
@@ -28,7 +28,72 @@ class PagesService extends Service {
         const group_id = { 
             url: "$url", 
         };
+        
+        return url ? await this.oneThread(queryjson, pageNo, pageSize, group_id)
+            : await this.moreThread(appId, type, beginTime, endTime, queryjson, pageNo, pageSize, group_id);
 
+    }
+
+    // 获得多个页面的平均性能数据
+    async moreThread(appId, type, beginTime, endTime, queryjson, pageNo, pageSize, group_id) {
+        const result = [];
+        let distinct = await this.ctx.model.Web.WebPages.distinct('url', queryjson.$match) || [];
+        let copdistinct = distinct;
+
+        const betinIndex = (pageNo - 1) * pageSize;
+        if (distinct && distinct.length) {
+            distinct = distinct.slice(betinIndex, betinIndex + pageSize);
+        }
+        const resolvelist = [];
+        for (let i = 0, len = distinct.length; i < len; i++) {
+            queryjson.$match.url = distinct[i];
+            resolvelist.push(
+                Promise.resolve(
+                    this.ctx.model.Web.WebPages.aggregate([
+                        { $match: { app_id: appId, url: distinct[i], speed_type: type, create_time: { $gte: new Date(beginTime), $lte: new Date(endTime) } } },
+                        {
+                            $group: {
+                                _id: group_id,
+                                count: { $sum: 1 },
+                                load_time: { $avg: "$load_time" },
+                                dns_time: { $avg: "$dns_time" },
+                                tcp_time: { $avg: "$tcp_time" },
+                                dom_time: { $avg: "$dom_time" },
+                                white_time: { $avg: "$white_time" },
+                                request_time: { $avg: "$request_time" },
+                                analysisDom_time: { $avg: "$analysisDom_time" },
+                                ready_time: { $avg: "$ready_time" },
+                            }
+                        },
+                    ])
+                )
+            )
+        }
+        const all = await Promise.all(resolvelist) || [];
+        all.forEach(item => {
+            result.push(item[0]);
+        })
+        result.sort(function (obj1, obj2) {
+            let val1 = obj1.count;
+            let val2 = obj2.count;
+            if (val1 < val2) {
+                return 1;
+            } else if (val1 > val2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+
+        return {
+            datalist: result,
+            totalNum: copdistinct.length,
+            pageNo: pageNo,
+        };
+    }
+
+    // 单个页面查询平均信息
+    async oneThread(queryjson, pageNo, pageSize, group_id) {
         const count = Promise.resolve(this.ctx.model.Web.WebPages.distinct('url', queryjson.$match));
         const datas = Promise.resolve(
             this.ctx.model.Web.WebPages.aggregate([
