b6848c7b8583f3bef0eefa4f18d75296419f71d5
wangweianger <752636052@qq.com>
2018-10-19 16:16:48 +0800

    新增demo图片

--- app/middleware/token_required.js
+++ app/middleware/token_required.js
@@ -3,12 +3,14 @@
 
 module.exports = () => {
     return async function(ctx, next) {
-        const usertoken = ctx.cookies.get('usertoken');
+        const usertoken = ctx.cookies.get('usertoken') || '';
+        console.log(usertoken)
         if (!usertoken) {
             ctx.body = {
                 code: 1004,
                 desc: '用户未登录',
             };
+            return;
         }
         const data = await ctx.service.user.finUserForToken(usertoken);
         if (!data || !data.user_name) {
@@ -16,6 +18,7 @@ module.exports = () => {
                 code: 1004,
                 desc: '登录用户无效！',
             };
+            return;
         }
         await next();
     };
