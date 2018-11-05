2806de53f892d1473d9e9dfc7db5ebaadd956a54
wangweianger <752636052@qq.com>
2018-11-05 17:34:47 +0800

    新增top分析列表

---
+++ app/model/Wx/wx_statis.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = app => {
+    const mongoose = app.mongoose;
+    const Schema = mongoose.Schema;
+    const conn = app.mongooseDB.get('db3');
+
+    const WxStatisSchema = new Schema({
+        app_id: { type: String }, // 所属系统ID
+        top_pages: { type: Array }, // top访问page列表
+        top_jump_out: { type: Array }, // top访问跳出率页面列表
+        create_time: { type: Date, default: Date.now },
+    });
+
+    WxStatisSchema.index({ app_id: 1, create_time: 1 });
+
+    return conn.model('WxStatis', WxStatisSchema);
+};
