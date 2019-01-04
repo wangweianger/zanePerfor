c3986c2b7d62ec325d3a3923dce795d3b25504c1
wangweianger <752636052@qq.com>
2019-01-04 10:39:49 +0800

    update mongodb default prot

--- config/config.prod.js
+++ config/config.prod.js
@@ -43,7 +43,7 @@ module.exports = () => {
     const dbclients = {
         db3: {
             // 单机部署
-            url: 'mongodb://127.0.0.1:27019/performance',
+            url: 'mongodb://127.0.0.1:27017/performance',
             // 副本集 读写分离
             // url: 'mongodb://127.0.0.1:28100,127.0.0.1:28101,127.0.0.1:28102/performance?replicaSet=rs1',
             // 集群分片
@@ -61,7 +61,7 @@ module.exports = () => {
     if (config.report_data_type === 'mongodb') {
         dbclients.db1 = {
             // url: 'mongodb://127.0.0.1:27017,127.0.0.1:27018/performance?replicaSet=performance',
-            url: 'mongodb://127.0.0.1:27017/performance',
+            url: 'mongodb://127.0.0.1:27019/performance',
             options: {
                 poolSize: 100,
                 keepAlive: 10000,
