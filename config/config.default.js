31d209d6d302c2d0da68471e771680df8febb102
wangweianger <752636052@qq.com>
2019-02-14 09:26:18 +0800

    default redis

--- config/config.default.js
+++ config/config.default.js
@@ -55,7 +55,7 @@ module.exports = () => {
     config.ip_thread = 5;
 
     // 上报原始数据使用redis存储、kafka储存、还是使用mongodb存储
-    config.report_data_type = 'kafka'; // redus  mongodb  kafka
+    config.report_data_type = 'redis'; // redus  mongodb  kafka
 
     // 使用redis储存原始数据时，相关配置 （report_data_type=redus生效）
     config.redis_consumption = {
