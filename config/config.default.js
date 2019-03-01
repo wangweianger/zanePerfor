b921f46076a33c6cc3f52e379422783922f4858f
wangweianger <752636052@qq.com>
2019-03-01 17:20:41 +0800

    简化kafka消费者调用方式

--- config/config.default.js
+++ config/config.default.js
@@ -56,7 +56,7 @@ module.exports = () => {
     config.ip_thread = 5;
 
     // 上报原始数据使用redis存储、kafka储存、还是使用mongodb存储
-    config.report_data_type = 'kafka'; // redis  mongodb  kafka
+    config.report_data_type = 'redis'; // redis  mongodb  kafka
 
     // 使用redis储存原始数据时，相关配置 （report_data_type=redis生效）
     config.redis_consumption = {
