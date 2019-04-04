706c779e13cdacdd11f5016f9038b0717d663ba5
wangweianger <752636052@qq.com>
2019-04-04 16:55:49 +0800

    关闭上报接口http:keep-alive

--- app/controller/api/web/report.js
+++ app/controller/api/web/report.js
@@ -12,6 +12,7 @@ class ReportController extends Controller {
         ctx.set('Access-Control-Allow-Origin', '*');
         ctx.set('Content-Type', 'application/json;charset=UTF-8');
         ctx.set('X-Response-Time', '2s');
+        ctx.set('Connection', 'close');
         ctx.status = 200;
 
         const query = ctx.request.body;
--- app/controller/api/wx/report.js
+++ app/controller/api/wx/report.js
@@ -10,6 +10,7 @@ class AjaxsController extends Controller {
         ctx.set('Access-Control-Allow-Origin', '*');
         ctx.set('Content-Type', 'application/json;charset=UTF-8');
         ctx.set('X-Response-Time', '2s');
+        ctx.set('Connection', 'close');
         ctx.status = 200;
 
         const query = ctx.request.body;
